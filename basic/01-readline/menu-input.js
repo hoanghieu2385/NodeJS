50fe9622c9efcebb3ee5a27d9dc85b0cd8aca94d
HoangHieu <hoangminhhieu230805@gmail.com>
2025-07-16 11:15:56 +0700

    get api

--- basic/01-readline/menu-input.js
+++ basic/01-readline/menu-input.js
@@ -1,9 +1,38 @@
+import { error } from 'console';
 import readline from 'readline';
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+
+function get_gold_price() {
+    const url = 'http://api.btmc.vn/api/BTMCAPI/getpricebtmc?key=3kd8ub1llcg9t45hnoh8hmn7t5kc2v'
+
+    fetch (url) .then (response => response.json())
+                .then (data => {
+                    console.log("Data get form api: ", JSON.stringify(data, null, 4));
+                    Menu();
+                })
+                .catch(error => {
+                    console.log("Error: ", error);
+                    Menu();
+                })
+}
+
+function get_usd_price() {
+    const url = 'https://open.er-api.com/v6/latest/USD'
+
+    fetch (url) .then (response => response.json())
+                .then (data => {
+                    console.log("Data get form api: ", JSON.stringify(data, null, 4));
+                    Menu();
+                })
+                .catch(error => {
+                    console.log("Error: ", error);
+                    Menu();
+                })
+}
 
 function Menu () {
     console.log('== MENU ==')
@@ -14,12 +43,12 @@ function Menu () {
     rl.question("Enter your choice: ", (choice) => {
         switch (choice) {
             case '1':
-                console.log("SJC: 119,500 VNĐ / GOLD")
-                break;
+                get_gold_price()
+                return;
 
             case '2':
-                console.log("1 USD = 26,115 VNĐ")
-                break;
+                get_usd_price();
+                return;
 
             case '3':
                 console.log("Exiting..")
@@ -34,5 +63,7 @@ function Menu () {
         Menu();
     });
 }
+
+
 
 Menu();
