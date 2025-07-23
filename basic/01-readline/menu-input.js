309ed11f0bc55a3638d871a0832cc2c28ef08b97
HoangHieu <hoangminhhieu230805@gmail.com>
2025-07-23 09:34:53 +0700

    Update function and variable names for ease of understanding

--- basic/01-readline/menu-input.js
+++ basic/01-readline/menu-input.js
@@ -1,86 +1,85 @@
 import readline from 'readline';
 
 const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
 
-function getGoldPrice() {
-	const url = 'http://api.btmc.vn/api/BTMCAPI/getpricebtmc?key=3kd8ub1llcg9t45hnoh8hmn7t5kc2v';
+function fetchGoldPricesFromBTMC() {
+  const apiUrl = 'http://api.btmc.vn/api/BTMCAPI/getpricebtmc?key=3kd8ub1llcg9t45hnoh8hmn7t5kc2v';
 
-	fetch(url)
-		.then((response) => response.json())
-		.then((data) => {
-			const goldItems = data.DataList.Data;
-			goldItems.forEach((items, index) => {
-                const i = index + 1
-				const name     = items[`@n_${i}`];
-				const k        = items[`@k_${i}`];
-				const content  = items[`@h_${i}`];
-				const buy      = items[`@pb_${i}`];
-				const sell     = items[`@ps_${i}`];
-				const time_get = items[`@d_${i}`];
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      const goldItems = data.DataList.Data;
+      goldItems.forEach((items, index) => {
+        const itemIndex = index + 1;
+        const goldTypeName = items[`@n_${itemIndex}`];
+        const goldKarats = items[`@k_${itemIndex}`];
+        const goldContent = items[`@h_${itemIndex}`];
+        const buyPrice = items[`@pb_${itemIndex}`];
+        const sellPrice = items[`@ps_${itemIndex}`];
+        const timestamp = items[`@d_${itemIndex}`];
 
-				console.log(`\nType of gold: ${name}`);
-				console.log(`Content : ${Number(content).toLocaleString()} ` + `(${k})`);
-				console.log(`Buy price: ${Number(buy).toLocaleString()} VND`);
-				console.log(`Sell price: ${Number(sell).toLocaleString()} VND`);
-				console.log(`Time: ${time_get}`);
-			});
-			Menu();
-		})
-		.catch((error) => {
-			console.log('Error: ', error);
-			Menu();
-		});
+        console.log(`\nType of gold: ${goldTypeName}`);
+        console.log(`Gold content : ${Number(goldContent).toLocaleString()} ` + `(${goldKarats})`);
+        console.log(`Buy price: ${Number(buyPrice).toLocaleString()} VND`);
+        console.log(`Sell price: ${Number(sellPrice).toLocaleString()} VND`);
+        console.log(`Last updated: ${timestamp}`);
+      });
+      Menu();
+    })
+    .catch((error) => {
+      console.log('An error occurred: ', error);
+      Menu();
+    });
 }
 
-function getUsdToVND() {
-	const url = 'https://open.er-api.com/v6/latest/USD';
+function fetchUsdToVndExchangeRate() {
+  const apiUrl = 'https://open.er-api.com/v6/latest/USD';
 
-	fetch(url)
-		.then((response) => response.json())
-		.then((data) => {
-			const vnd = data.rates['VND'];
-			console.log(`1 USD = ${vnd} VND`);
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      const exchangeRateVnd = data.rates['VND'];
+      console.log(`1 USD = ${exchangeRateVnd} VND`);
 
-			Menu();
-		})
-
-		.catch((error) => {
-			console.log('Error: ', error);
-			Menu();
-		});
+      Menu();
+    })
+    .catch((error) => {
+      console.log('An error occurred: ', error);
+      Menu();
+    });
 }
 
 function Menu() {
-	console.log('\n== MENU ==');
-	console.log('1. Gold price');
-	console.log('2. USD price');
-	console.log('3. Exit');
+  console.log('\n== MENU ==');
+  console.log('1. View Gold Prices');
+  console.log('2. View USD to VND Exchange Rate');
+  console.log('3. Exit');
 
-	rl.question('Enter your choice: ', (choice) => {
-		switch (choice) {
-			case '1':
-				getGoldPrice();
-				return;
+  rl.question('Enter your choice: ', (choice) => {
+    switch (choice) {
+      case '1':
+        fetchGoldPricesFromBTMC();
+        return;
 
-			case '2':
-				getUsdToVND();
-				return;
+      case '2':
+        fetchUsdToVndExchangeRate();
+        return;
 
-			case '3':
-				console.log('Exiting..');
-				rl.close();
-				return;
+      case '3':
+        console.log('Exiting..');
+        rl.close();
+        return;
 
-			default:
-				console.log('Please choice a valid input!!');
-				break;
-		}
+      default:
+        console.log('Please choice a valid input!!');
+        break;
+    }
 
-		Menu();
-	});
+    Menu();
+  });
 }
 
 Menu();
